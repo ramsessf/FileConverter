10380c52bcb569fef10af0457c9e2ef435597a7f
Ramses Farias <60299087+ramsessf@users.noreply.github.com>
2021-09-25 13:13:51 -0300

    updated frontend
    created navbar

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/ramsessf" target="_blank" rel="noreferrer">Ramses Farias</a></p>
+                <p className="text-light"><small><strong>Projeto FileConverter API</strong><br />
+      Conversor de arquivos de imagem para o formato Base64: <a href="https://www.linkedin.com/in/ramsesfarias89/" target="_blank" rel="noreferrer">@ramsesfarias89</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,33 @@
+import ImgDsDark from 'assets/img/Online-File-Converter.jpg';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src={ImgDsDark} alt="Online-File-Converterr" width="120" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
